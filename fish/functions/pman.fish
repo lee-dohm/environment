84b523f78a75d587a1eb483b30788f107c2674bd
Lee Dohm <lee@liftedstudios.com>
2013-08-24 16:21:30 -0700

    Use standard format for tests

--- fish/functions/pman.fish
+++ fish/functions/pman.fish
@@ -1,6 +1,6 @@
 
 function pman --description 'Opens a man page in the OS X Preview app'
-    if [ (uname -s) = 'Darwin' ]
+    if test (uname -s) = 'Darwin'
         man -t $argv[1] | open -f -a /Applications/Preview.app
     else
         man $argv
