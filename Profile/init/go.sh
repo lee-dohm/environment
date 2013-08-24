9a5be1a5be458dbebf7875185b2d2ea614444140
Lee Dohm <lee@liftedstudios.com>
2013-08-23 20:47:17 -0700

    Made the goplay alias simpler by using GOROOT in the path

--- Profile/init/go.sh
+++ Profile/init/go.sh
@@ -4,5 +4,5 @@ if [[ -e "/usr/local/bin/go" ]] then
     export GOROOT="/usr/local/Cellar/go/1.1"
     export GOPATH="$SOURCE_DIR/gopath"
     export PATH="$PATH:$GOPATH/bin"
-    alias goplay="go run /usr/local/Cellar/go/1.1/misc/goplay/goplay.go"
+    alias goplay="go run $GOROOT/misc/goplay/goplay.go"
 fi
