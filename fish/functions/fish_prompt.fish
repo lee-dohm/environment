87fbc43377f473f07c162affcbfc89453bd01362
Lee Dohm <lee@liftedstudios.com>
2013-08-24 13:16:04 -0700

    Make prompts almost identical to preferred zsh prompts

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -71,14 +71,14 @@ function fish_prompt --description 'Write out the prompt'
 	end
 
 	if not set -q __fish_prompt_user
-		set -g __fish_prompt_user (set_color $fish_color_user)
+		set -g __fish_prompt_user (set_color normal; set_color $fish_color_user)
 	end
 	if not set -q __fish_prompt_host
-		set -g __fish_prompt_host (set_color $fish_color_host)
+		set -g __fish_prompt_host (set_color normal; set_color $fish_color_host)
 	end
     if not set -q __fish_prompt_punc
         set -g __fish_prompt_punc (set_color --bold white)
     end
 
-	echo -n -s "$__fish_prompt_punc" "[$__fish_prompt_user" "$USER" "$__fish_prompt_punc" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_punc" ']' "$__fish_prompt_normal" (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" " $delim" ' '
+	echo -n -s "$__fish_prompt_punc" "[$__fish_prompt_user" "$USER" "$__fish_prompt_punc" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_punc" ']' "$__fish_prompt_normal" (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" " $delim "
 end
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,4 +1,5 @@
 
 function fish_right_prompt
-    echo (echo "[$PWD]" | sed -e "s|^\[$HOME|[~|" -e 's|^/private||')
+    echo (echo "$__fish_prompt_punc""[$__fish_prompt_normal$PWD$__fish_prompt_punc""]" | sed -e "s|$HOME|~|" -e 's|^/private||')
+    set_color normal
 end
