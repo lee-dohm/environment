7010c80b5fe7edc75a7bac45d4a10720b3de894e
Lee Dohm <lee@liftedstudios.com>
2013-08-24 13:44:33 -0700

    Simplify the startup script even more

--- Profile/config.fish
+++ Profile/config.fish
@@ -1,9 +1,7 @@
 
-set --global --export SCRIPT_DIR (dirname (dirname (status -f)))
-set --global --export SOURCE_DIR (dirname $SCRIPT_DIR)
 set --global --export PROFILE (status -f)
-
-set --local os (uname -s)
+set --global --export SCRIPT_DIR (dirname (dirname $PROFILE))
+set --global --export SOURCE_DIR (dirname $SCRIPT_DIR)
 
 if not contains $SCRIPT_DIR/fish/functions $fish_function_path
     set fish_function_path $SCRIPT_DIR/fish/functions $fish_function_path
