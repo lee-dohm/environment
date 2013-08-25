cf66b27b994c728fc28f57f4a922be93b28418e5
Lee Dohm <lee@liftedstudios.com>
2013-08-24 19:32:25 -0700

    Cleaned up the code in the absolute_path function

--- fish/functions/absolute_path.fish
+++ fish/functions/absolute_path.fish
@@ -1,5 +1,7 @@
 
 function absolute_path --description 'Converts a relative path into an absolute path'
-    set --local dst $argv[1]
-    cd (dirname $dst) > /dev/null; and echo (pwd -P)/(basename $dst); and popd
+    set --local target $argv[1]
+    set --local curdir (pwd)
+
+    builtin cd (dirname $target) > /dev/null; and echo (pwd -P)/(basename $target); and builtin cd $curdir
 end
