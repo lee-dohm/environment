38b888153d8fa461d133387e3f38cb7b14bfffdd
Lee Dohm <lee@liftedstudios.com>
2013-08-24 20:11:31 -0700

    Change goplay from alias to explicit function

--- fish/init/go.fish
+++ fish/init/go.fish
@@ -8,5 +8,8 @@ if which go > /dev/null
     set --global --export GOROOT $go_dir
     set --global --export GOPATH $SOURCE_DIR/gopath
     append_path_entry $GOPATH/bin
-    alias goplay="go run $GOROOT/misc/goplay/goplay.go"
+
+    function goplay --description 
+        go run $GOROOT/misc/goplay/goplay.go $argv
+    end
 end
