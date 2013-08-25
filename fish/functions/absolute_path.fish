64a0e43a3197492b8d0d19e0902b400ca72b19fb
Lee Dohm <lee@liftedstudios.com>
2013-08-24 19:54:42 -0700

    Change to always change back to original directory

--- fish/functions/absolute_path.fish
+++ fish/functions/absolute_path.fish
@@ -3,5 +3,6 @@ function absolute_path --description 'Converts a relative path into an absolute 
     set --local target $argv[1]
     set --local curdir (pwd)
 
-    builtin cd (dirname $target) > /dev/null; and echo (pwd)/(basename $target); and builtin cd $curdir
+    builtin cd (dirname $target) > /dev/null; and echo (pwd)/(basename $target)
+    builtin cd $curdir
 end
