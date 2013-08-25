ab7e2f123bd87a36e6dd3652d68bbbd696d0df3e
Lee Dohm <lee@liftedstudios.com>
2013-08-24 18:49:22 -0700

    Customize the git portion of the prompt

---
+++ fish/init/git.fish
@@ -0,0 +1,10 @@
+
+set -e __fish_git_prompt_showdirtystate
+set -e __fish_git_prompt_showuntrackedfiles
+
+set --global --export __fish_git_prompt_showstashstate 1
+
+set --global --export __fish_git_prompt_color 'normal'
+set --global --export __fish_git_prompt_color_branch 'yellow'
+set --global --export __fish_git_prompt_color_prefix '-o' 'white'
+set --global --export __fish_git_prompt_color_suffix '-o' 'white'
