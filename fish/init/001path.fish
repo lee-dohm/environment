3a8a7b5fa618ab55a2144c0952b8ff54dc03fefe
Lee Dohm <lee@liftedstudios.com>
2013-08-25 20:27:36 -0700

    Fix potentially uninitialized variable.

--- fish/init/001path.fish
+++ fish/init/001path.fish
@@ -12,8 +12,8 @@ set --local local_bin_index (contains --index /usr/local/bin $PATH)
 
 if test $local_bin_index -gt $bin_index
     set --erase $PATH[$local_bin_index]
-    set --local --erase local_path
-    
+
+    set --local local_path ''
     if test (math $bin_index - 1) -gt 0
         set local_path $PATH[1..(math $bin_index - 1)]
     end
