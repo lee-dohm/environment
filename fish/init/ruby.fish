49bb5572c70114ebb7b53ce06d50cfeea8e8a934
Lee Dohm <lee@liftedstudios.com>
2013-08-26 14:32:16 -0700

    Check for existence of rvm function and install if not

--- fish/init/ruby.fish
+++ fish/init/ruby.fish
@@ -1,2 +1,6 @@
 
+if not functions --query rvm
+    curl --create-dirs -o ~/.config/fish/functions/rvm.fish https://raw.github.com/lunks/fish-nuggets/master/functions/rvm.fish
+end
+
 rvm use default > /dev/null
