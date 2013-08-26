229ae1085308467fb29850ad8d72cc74bc8037f9
Lee Dohm <lee@liftedstudios.com>
2013-08-26 12:24:39 -0700

    Add check to ensure default colors

---
+++ fish/init/colors.fish
@@ -0,0 +1,8 @@
+
+if [ "$fish_color_user" != 'green' ]
+    set --universal --export fish_color_user green
+end
+
+if [ "$fish_color_host" != 'yellow' ]
+    set --universal --export fish_color_host yellow
+end
