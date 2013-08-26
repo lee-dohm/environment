1e6422aa900913b4e71baf79ac04f27f5be793d0
Lee Dohm <lee@liftedstudios.com>
2013-08-26 12:38:34 -0700

    Simplify colors.fish init script.
    This is in preparation for making colors.fish set all of my standard
    environment's colors. Added a new ensure_color function to
    make the transition simple.

---
+++ fish/functions/ensure_color.fish
@@ -0,0 +1,15 @@
+
+##
+# @param var_name       Name of the environment variable that holds the color information.
+# @param set_color_args Arguments to pass to `set_color` when using this color value.
+function ensure_color --description 'Ensures that the given color name is set to the supplied set_color argument array'
+    set --local var_name $argv[1]
+    set --local set_color_args $argv[2..-1]
+    set --local var_value $$var_name
+
+    if [ "$var_value" != "$set_color_args" ]
+        set --universal $var_name $set_color_args
+    end
+
+    return 0
+end
--- fish/init/colors.fish
+++ fish/init/colors.fish
@@ -1,8 +1,3 @@
 
-if [ "$fish_color_user" != 'green' ]
-    set --universal --export fish_color_user green
-end
-
-if [ "$fish_color_host" != 'yellow' ]
-    set --universal --export fish_color_host yellow
-end
+ensure_color fish_color_user green
+ensure_color fish_color_host yellow
