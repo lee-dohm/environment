e3ab8fc5339c8cadc01f23df290bb45274089ebb
Lee Dohm <lee@liftedstudios.com>
2013-08-26 19:29:49 -0700

    Make .Rprofile creation calculate the proper path

--- fish/init/R.fish
+++ fish/init/R.fish
@@ -1,13 +1,13 @@
 
-begin
-    set --local r_profile ~/.Rprofile
+set --local r_profile ~/.Rprofile
 
-    if [ (which R > /dev/null) -a not -f $r_profile ]
-        echo >> $r_profile
-        echo '.First <- function() {' >> $r_profile
-        echo '  Sys.setenv("R_LIB_PATH"="~/Source/environment/R")' >> $r_profile
-        echo '  source("~/Source/environment/R/profile.R")' >> $r_profile
-        echo '}' >> $r_profile
-        echo >> $r_profile
-    end
+if [ (which R > /dev/null) -a (not [ -f $r_profile ]) ]
+    set --local r_home $SCRIPT_DIR/R
+
+    echo >> $r_profile
+    echo '.First <- function() {' >> $r_profile
+    echo "    Sys.setenv('R_LIB_PATH'='$r_home')" >> $r_profile
+    echo "    source('$r_home/profile.R')" >> $r_profile
+    echo '}' >> $r_profile
+    echo >> $r_profile
 end
