eedb481041be3c1b5c6bcc394a6ed472c5470f00
Lee Dohm <lee@liftedstudios.com>
2013-09-01 12:36:37 -0700

    Add PostgreSQL startup and shutdown functions

--- fish/init/postgresql.fish
+++ fish/init/postgresql.fish
@@ -1,5 +1,17 @@
 
 if which postgres > /dev/null
-    set --global --export POSTGRES (get_active_brew_dir (which postgres))
-    set --global --export POSTGRES_BIN $POSTGRES/bin
+    set --global --export POSTGRES_DATA /usr/local/var/postgres
+
+    function start_postgres
+        set --local logfile $argv[1]
+        if [ -z $logfile ]
+            set logfile ~/postgres.log
+        end
+
+        pg_ctl start --pgdata $POSTGRES_DATA --log $logfile
+    end
+
+    function stop_postgres
+        pg_ctl stop --pgdata $POSTGRES_DATA
+    end
 end
