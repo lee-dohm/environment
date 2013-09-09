7cf05753b9d47a21f8b9e09389b0cce9c329e758
Lee Dohm <lee@liftedstudios.com>
2013-09-09 11:35:50 -0700

    Use full parameter name for clarity

--- fish/init/editor.fish
+++ fish/init/editor.fish
@@ -1,6 +1,6 @@
 
 if [ (uname -s) = 'Darwin' -a -f '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' ]
-    set --global --export EDITOR 'subl -w'
+    set --global --export EDITOR 'subl --wait'
 else
     set --global --export EDITOR vim
 end
