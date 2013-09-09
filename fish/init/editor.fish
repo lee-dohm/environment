4c390bb0a20305f6b71cfef5df08c342de879bb8
Lee Dohm <lee@liftedstudios.com>
2013-09-09 11:33:15 -0700

    Add check to see if Sublime Text is installed before setting EDITOR

--- fish/init/editor.fish
+++ fish/init/editor.fish
@@ -1,5 +1,5 @@
 
-if [ (uname -s) = 'Darwin' ]
+if [ (uname -s) = 'Darwin' -a -f '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' ]
     set --global --export EDITOR 'subl -w'
 else
     set --global --export EDITOR vim
