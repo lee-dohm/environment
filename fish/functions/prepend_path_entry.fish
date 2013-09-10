ffc4ba1c6ff541fb347744dbd844ba96757719e3
Lee Dohm <lee@liftedstudios.com>
2013-09-10 09:53:56 -0700

    Remove unused and buggy function

--- fish/functions/prepend_path_entry.fish
+++
@@ -1,8 +0,0 @@
-
-function prepend_path_entry --description 'Prepends an entry or list of entries onto the PATH'
-    for entry in $argv
-        if not contains $entry $PATH
-            set PATH $entry $PATH
-        end
-    end
-end
