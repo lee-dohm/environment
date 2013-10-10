9138918b0d756f1b1bab34a7c89cc580368f8e9b
Lee Dohm <lee@liftedstudios.com>
2013-10-09 19:38:59 -0700

    Convert from rvm to rbenv

--- fish/init/ruby.fish
+++ fish/init/ruby.fish
@@ -1,11 +1,41 @@
 
-set --global __rvm_function_path ~/.config/fish/functions/rvm.fish
-set --global __rvm_function_repo https://raw.github.com/lunks/fish-nuggets/master/functions/rvm.fish
+set --export PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1
 
-if not functions --query rvm; or should_update_rvm_function
-    update_rvm_function
+function rbenv_shell
+  set --local vers $argv[1]
+
+  switch "$vers"
+    case '--complete'
+      echo '--unset'
+      echo 'system'
+      exec rbenv-versions --bare
+      return
+    case '--unset'
+      set --erase RBENV_VERSION
+    case ''
+      if [ -z "$RBENV_VERSION" ]
+        echo "rbenv: no shell-specific version configured" >&2
+        return 1
+      else
+        echo "$RBENV_VERSION"
+      end
+    case '*'
+      rbenv prefix "$vers" > /dev/null
+      set --global --export RBENV_VERSION "$vers"
+  end
 end
 
-rvm use default > /dev/null
+function rbenv
+  set -l command $argv[1]
+  [ (count $argv) -gt 1 ]; and set -l args $argv[2..-1]
+
+  switch "$command"
+    case shell
+      rbenv_shell $args
+    case '*'
+      command rbenv $command $args
+  end
+end
 
 set --global --export RAILS_ENV development
