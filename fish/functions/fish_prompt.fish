3a8ae2619590b5544648eb12720edf00e7ab6d29
Lee Dohm <lee@liftedstudios.com>
2013-10-23 19:18:12 -0700

    Experiment with powerline shell prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,66 +1,14 @@
+
 function fish_prompt --description 'Write out the prompt'
+    set -l last_status $status
 
-	set -l last_status $status
-
-	# Just calculate these once, to save a few cycles when displaying the prompt
-	if not set -q __fish_prompt_hostname
-		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	end
-
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
-	end
-
-	if not set -q -g __fish_classic_git_functions_defined
-		set -g __fish_classic_git_functions_defined
-
-		function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-			if status --is-interactive
-				set -e __fish_prompt_user
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-			if status --is-interactive
-				set -e __fish_prompt_host
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
-			if status --is-interactive
-				set -e __fish_prompt_status
-				commandline -f repaint ^/dev/null
-			end
-		end
-	end
-
-    set --local delim ''
-
-    if [ (id -u) -eq (id -u root) ]
-        set delim (set_color red)'%'(set_color normal)
-    else
-        set delim '$'
-	end
-
-	set -l prompt_status
-	if test $last_status -ne 0
-		if not set -q __fish_prompt_status
-			set -g __fish_prompt_status (set_color $fish_color_status)
-		end
-		set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
-	end
-
-	if not set -q __fish_prompt_user
-		set -g __fish_prompt_user (set_color normal; set_color $fish_color_user)
-	end
-	if not set -q __fish_prompt_host
-		set -g __fish_prompt_host (set_color normal; set_color $fish_color_host)
-	end
     if not set -q __fish_prompt_punc
         set -g __fish_prompt_punc (set_color --bold white)
     end
 
-	echo -n -s "$__fish_prompt_punc" "[$__fish_prompt_user" "$USER" "$__fish_prompt_punc" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_punc" ']' "$__fish_prompt_normal" (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" " $delim "
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    ~/.powerline-shell.py $last_status --shell bare
 end
