d3e28bfaa1698a5f9942808b52a6b8a2b32ad5d7
Lee Dohm <lee@liftedstudios.com>
2013-11-17 23:08:36 -0800

    Use Emacs in daemon mode

--- fish/init/editor.fish
+++ fish/init/editor.fish
@@ -1,6 +1,12 @@
 
 if which emacs > /dev/null
-    set --global --export EDITOR emacs
+    set --global --export EDITOR "emacsclient -t"
+    set --global --export ALTERNATE_EDITOR ''
+
+    alias e='emacsclient -t'
+    alias ec='emacsclient -c'
+    alias vim='emacsclient -t'
+    alias vi='emacsclient -t'
 else if which subl > /dev/null
     set --global --export EDITOR 'subl --wait'
 else
