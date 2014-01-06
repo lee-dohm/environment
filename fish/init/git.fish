6d6ee8f3b7ee8bf7f0dd8ca43789a73f6b84ed93
Lee Dohm <lee@liftedstudios.com>
2014-01-06 07:34:45 -0800

    Alias git to hub when hub is installed

--- fish/init/git.fish
+++ fish/init/git.fish
@@ -8,3 +8,9 @@ set --global --export __fish_git_prompt_color 'normal'
 set --global --export __fish_git_prompt_color_branch 'yellow'
 set --global --export __fish_git_prompt_color_prefix '-o' 'white'
 set --global --export __fish_git_prompt_color_suffix '-o' 'white'
+
+if which hub > /dev/null
+    function git --description "Alias git to hub"
+        hub $argv
+    end
+end
