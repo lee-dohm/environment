4f96461ed8e4284a6dffe77b16368b528c1d49f8
Lee Dohm <lee@liftedstudios.com>
2014-01-08 17:12:06 -0800

    Clean up prompt code

--- zsh/init/prompt.sh
+++ zsh/init/prompt.sh
@@ -2,13 +2,18 @@ autoload -U promptinit
 promptinit
 
 # Set the prompt that I like
-precmd()
-{
-    local branch=''
-    local exit=''
+precmd () {
     branch=`git branch --no-color 2>/dev/null | grep \* | sed "s/^\* \(.*\)$/(${YELLOW}\1${BOLD_WHITE}) /"`
+
     exit="%(?..${RED}[%?]${RESET} )"
-    PS1="${BOLD_WHITE}[${GREEN}%n${BOLD_WHITE}@${YELLOW}%m${BOLD_WHITE}] $branch$exit%#${RESET} "
+
+    sep="${BOLD_WHITE}%#${RESET} "
+
+    host="${YELLOW}%m"
+    user="${GREEN}%n"
+    loc="${BOLD_WHITE}[$user${BOLD_WHITE}@$host${BOLD_WHITE}] "
+
+    PS1="$loc$branch$exit$sep"
 }
 
 RPROMPT="${BOLD_WHITE}[${WHITE}%~${BOLD_WHITE}]${RESET}"
