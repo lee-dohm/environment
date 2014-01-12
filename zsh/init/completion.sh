667114df582c9e9da320216537b9daa493b3b59c
Lee Dohm <lee@liftedstudios.com>
2014-01-11 22:05:29 -0800

    Add fuzzy completion capability

--- zsh/init/completion.sh
+++ zsh/init/completion.sh
@@ -1,2 +1,14 @@
 autoload -U compinit
 compinit
+
+# Fuzzy matching on completions
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
+# Allow number of errors to grow based on length of what is already typed
+zstyle -e ':completion:*:approximate:*' \
+       max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
+
+# Ignore completion functions for commands I don't have
+zstyle ':completion:*:functions' ignored-patterns '_*'
