a3cc95f57531d8e189eb6138d601909d98399f0f
Lee Dohm <lee@liftedstudios.com>
2014-01-12 19:54:12 -0800

    Add auto_cd option

--- zsh/init/cd.sh
+++ zsh/init/cd.sh
@@ -1,3 +1,4 @@
+setopt auto_cd       # Change directories by just typing the path without `cd`
 setopt autopushd     # Make cd work like pushd always
 setopt pushdminus    # Allow for expanding the directory history on the command line
 setopt pushdsilent   # Keeps the cd/pushd command from echoing the old directory each time
