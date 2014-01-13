99a80c5e6aa304a0fc7b97f74a618fb91fc51f81
Lee Dohm <lee@liftedstudios.com>
2014-01-13 11:03:12 -0800

    Fix bug in path for local init scripts

--- zsh/init/local-only.sh
+++ zsh/init/local-only.sh
@@ -2,7 +2,7 @@ if [ -d $SOURCE_DIR/local-only ]; then
     __append-path-entry $SOURCE_DIR/local-only/bin
 
     # Load any local only initializations
-    for init in `ls -d1 $SCRIPT_DIR/local-only/zsh/init/*`
+    for init in `ls -d1 $SOURCE_DIR/local-only/zsh/init/*`
     do
         source $init
     done
